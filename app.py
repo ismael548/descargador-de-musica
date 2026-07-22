4605489daa5641f748e3c8e012ab1d53ba6e94ef
ismael548 <jesusrodrigoquinobernabe1@gmail.com>
2026-07-22 13:52:19 -0500

    extractor

--- app.py
+++ app.py
@@ -22,8 +22,7 @@ def descargar_audio(url):
             'preferredcodec': 'mp3',
             'preferredquality': '192',
         }],
-        'cookiesfrombrowser': None,
-'extractor_args': {'youtube': {'player_client': ['android']}},
+        'extractor_args': {'youtube': {'player_client': ['android']}},
     }
     
     with yt_dlp.YoutubeDL(opciones) as ydl:
